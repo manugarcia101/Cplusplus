3bde8e2f3ce226da54b99cd4e0a4d2ba59ee4d6a
Manuel Garcia Lopez <manugarcial96@gmail.com>
2018-06-07 19:47:47 +0200

    Sorted and reordered

---
+++ Data Structures/Mapas STL/Practica 6 Mapas/Practica 6 Mapas/TextoPredictivo.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include "TextoPredictivo.h"
+
+
+TextoPredictivo::TextoPredictivo(Diccionario *dicIdioma) : miDiccBase(dicIdioma), misUsuarios()
+{
+}
+
+TextoPredictivo::TextoPredictivo(const TextoPredictivo &orig) : miDiccBase(orig.miDiccBase), misUsuarios(orig.misUsuarios)
+{
+}
+
+TextoPredictivo::~TextoPredictivo()
+{
+}
+
+std::list<Sucesor> TextoPredictivo::sugerencia(std::string &cadena) {
+	static std::list<Sucesor> misSugerencias;
+	misSugerencias = miDiccBase->mostrarPrioritarios(cadena);
+	return misSugerencias;
+}
+
+void TextoPredictivo::entrena(std::string frase) {
+	miDiccBase->entrena(frase);
+}
+
+bool TextoPredictivo::entrenaNuevo(std::string termino1, std::string termino2) {
+	bool estatermino1 = miDiccBase->buscaDicotomica(termino1), estatermino2 = miDiccBase->buscaDicotomica(termino2);
+	if (estatermino1 && estatermino2) {
+		miDiccBase->entrenaNuevo(termino1, termino2);
+		return true;
+	}
+	else return false;
+}
+
+bool TextoPredictivo::nuevoUsuario(const std::string &id, const std::string &nombre) {
+
+	std::pair<std::map<std::string, Usuario>::iterator, bool> miPair = misUsuarios.insert(std::pair<std::string, Usuario>(id, Usuario(id, nombre, this)));
+
+	//Usuario usuario(id, nombre, this);
+	//misUsuarios[id] = usuario; 
+
+	//return miPair.second;
+	return true;
+
+}
+
+Usuario* TextoPredictivo::obtenerUsuario(const std::string &id) {
+	
+	std::map<std::string, Usuario>::iterator miIt = misUsuarios.find(id);
+	if (miIt == misUsuarios.end()) {
+
+		throw std::out_of_range("Out of range in obtenerUsuario");
+	}
+	return &(miIt->second);
+}
+
+bool TextoPredictivo::busca(std::string &termino) {
+	bool esta = miDiccBase->buscaDicotomica(termino);
+	return esta;
+}
